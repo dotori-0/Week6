a67b7d65bbf9340cb3099b463c99cc0bde35adb7
dotori-0 <sesan131@gmail.com>
2022-08-10 17:32:05 +0900

    [Add] Reusable View Protocol

---
+++ Week6/Protocol/ReusableViewProtocol.swift
@@ -0,0 +1,25 @@
+//
+//  ReusableViewProtocol.swift
+//  Week6
+//
+//  Created by SC on 2022/08/10.
+//
+
+import UIKit
+
+
+protocol ReusableViewProtocol {
+    static var reuseIdentifier: String { get }  // 저장 프로퍼티이든 연산프로퍼티이든 상관없다
+}
+
+extension UICollectionViewCell: ReusableViewProtocol {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReusableViewProtocol {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
